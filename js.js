d2de038e60a114f9bdcec2b5f85598c8d6d9c668
Luancr772 <75171462+Luancr772@users.noreply.github.com>
2022-11-17 10:04:03 +0700

    form

--- js.js
+++ js.js
@@ -3,3 +3,15 @@ const btn = document.querySelector("button");
 btn.addEventListener("click", function () {
   alert("Logged in successfully");
 });
+
+const toggle = document.querySelector(".icon");
+
+toggle.addEventListener("click", function () {
+  const input = this.previousElementSibling;
+  const inputType = input.getAttribute("type");
+  if (inputType === "password") {
+    input.setAttribute("type", "text");
+  } else {
+    input.setAttribute("type", "password");
+  }
+});
